02cedc2219466c866a37bfa6a11fdafc176472b5
anima-libera <anim.libera@gmail.com>
2022-03-28 01:12:47 +0200

    Added version printing (Indev)

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,15 @@
 
 #include "config.hpp"
 #include "gameloop.hpp"
+#include <iostream>
 #include <cstdlib>
 
 using namespace qwy2;
 
 int main(int argc, char const* const* argv)
 {
+	std::cout << "Qwy2 - Version Indev (0.0.0)" << std::endl;
+
 	Config config{};
 	if (config.parse_command_line(argc, argv) == ErrorCode::ERROR)
 	{
